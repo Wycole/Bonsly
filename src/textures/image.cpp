2422f73d8daa3794b77f32e6712e64b54ff2e89e
Tolga Onay MAC <toon00001@stud.uni-saarland.de>
2024-11-18 15:37:23 +0100

    ducks test passes but the image test fails, I believe the clamp border mode doesnt work

--- src/textures/image.cpp
+++ src/textures/image.cpp
@@ -41,21 +41,58 @@ public:
     }
 
     Color evaluate(const Point2 &uv) const override {
+        float tx; // x of the texel
+        float ty; // y of the texel
+        float fracx;
+        float fracy;
         switch (m_border) {
         case BorderMode::Clamp: {
-            // not sure how to do this one
-            return Color(m_image->get(Point2i(uv.x(), uv.y())));
+            // get the floating point part of the number,
+            // add 1 if negative, if positive just keep going
+            float fracx = uv.x() - floor(uv.x());
+            float fracy = ceil(uv.y()) - uv.y(); // this was the problem
+            // dont forget that the y-axis is reversed since the origin is
+            // top left, not bot left
+            // look at how its defined in the assignment for the uv stuff
+            tx = (int) fmin(m_image->resolution().x() * fracx,
+                            m_image->resolution().x() - 1);
+            ty = (int) fmin(m_image->resolution().y() * fracy,
+                            m_image->resolution().y() - 1);
         }
 
         case BorderMode::Repeat: {
             // get the floating point part of the number,
             // add 1 if negative, if positive just keep going
-            float fracx = uv.x() - (int) (uv.x());
-            float fracy = uv.y() - (int) (uv.y());
-            return Color(m_image->get(
-                Point2i(m_image->resolution().y() * fracx + fracy)));
+            float fracx = uv.x() - floor(uv.x());
+            float fracy = ceil(uv.y()) - uv.y(); // this was the problem
+            // dont forget that the y-axis is reversed since the origin is
+            // top left, not bot left
+            // look at how its defined in the assignment for the uv stuff
+            tx = m_image->resolution().x() * fracx;
+            ty = m_image->resolution().y() * fracy;
+
+            // res x is width, y is height. at least I hope so
         }
         }
+        Color textured;
+        switch (m_filter) {
+        case FilterMode::Bilinear: {
+            // first in x, then in y
+            Color c00 = Color(m_image->get(Point2i(tx, ty)));
+            Color c10 = Color(m_image->get(Point2i(tx + 1, ty)));
+            Color c01 = Color(m_image->get(Point2i(tx, ty + 1)));
+            Color c11 = Color(m_image->get(Point2i(tx + 1, ty + 1)));
+            textured  = ((1 - fracx) * (1 - fracy) * c00) +
+                       ((fracx) * (1 - fracy) * c10) +
+                       ((1 - fracx) * (fracy) *c01) + ((fracx) * (fracy) *c11);
+        }
+
+        case FilterMode::Nearest: {
+            textured = Color(m_image->get(Point2i(tx, ty)));
+            // this is enough, just goes to the nearest texel to the point?
+        }
+        }
+        return textured;
     }
 
     std::string toString() const override {
