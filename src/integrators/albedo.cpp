e3d6a2db98b5bb077b515e9c61edc63cab72a2a7
Yun-Jhu Wang <yuwa00001@stud.uni-saarland.de>
2025-02-02 12:56:23 +0100

    comment on albedo integrator, still build failed?

--- src/integrators/albedo.cpp
+++ src/integrators/albedo.cpp
@@ -1,40 +1,40 @@
-#include <lightwave.hpp>
+// #include <lightwave.hpp>
 
-namespace lightwave {
+// namespace lightwave {
 
-class AlbedoIntegrator : public SamplingIntegrator {
+// class AlbedoIntegrator : public SamplingIntegrator {
 
-public:
-    AlbedoIntegrator(const Properties &properties)
-        : SamplingIntegrator(properties) {}
+// public:
+//     AlbedoIntegrator(const Properties &properties)
+//         : SamplingIntegrator(properties) {}
 
-    Color Li(const Ray &ray, Sampler &rng) override {
+//     Color Li(const Ray &ray, Sampler &rng) override {
 
-        Intersection intersection = m_scene->intersect(ray, rng);
+//         Intersection intersection = m_scene->intersect(ray, rng);
 
-        // if no intersection, return background color
-        if (!intersection) {
-            return intersection.evaluateEmission().value;
-        }
+//         // if no intersection, return background color
+//         if (!intersection) {
+//             return intersection.evaluateEmission().value;
+//         }
 
-        // if (!intersection || !intersection.instance->bsdf()){
-        //     return Color(0.f);
-        // }
+//         // if (!intersection || !intersection.instance->bsdf()){
+//         //     return Color(0.f);
+//         // }
 
-        // return albedo evaluated at uv
-        return Color(intersection.wo); // ???????
-    }
+//         // return albedo evaluated at uv
+//         return Color(intersection.wo); // ???????
+//     }
 
-    std::string toString() const override {
-        return tfm::format(
-            "AlbedoIntegrator[\n"
-            " sampler = %s,\n"
-            " image =%s, \n"
-            "]",
-            indent(m_sampler),
-            indent(m_image));
-    }
-};
-} // namespace lightwave
+//     std::string toString() const override {
+//         return tfm::format(
+//             "AlbedoIntegrator[\n"
+//             " sampler = %s,\n"
+//             " image =%s, \n"
+//             "]",
+//             indent(m_sampler),
+//             indent(m_image));
+//     }
+// };
+// } // namespace lightwave
 
-REGISTER_INTEGRATOR(AlbedoIntegrator, "albedo")
+// REGISTER_INTEGRATOR(AlbedoIntegrator, "albedo")
